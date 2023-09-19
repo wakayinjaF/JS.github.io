38ce92a96b45074d6a62812798bfc487468cfa13
wakayinja frank <142891493+wakayinjaF@users.noreply.github.com>
2023-09-19 11:31:07 +0300

    Update New.js

--- New.js
+++ New.js
@@ -6,20 +6,21 @@ console.log('Hello world');
 //arrays 
 let selectedName = [
   {
-    name='jemmy',
-    heigth='23',
-    age='45',
+    name: 'jemmy',
+    height: '23',
+    age: '45',
   },
   {
-    name='sammie',
-    height='178',
-    age='56',
+    name: 'sammie',
+    height: '178',
+    age: '56',
   },
   {
-    name='jack',
-    height='78',
-    age='12',
+    name: 'jack',
+    height: '78',
+    age: '12',
   },
 ];
 
 console.log(selectedName[1]);
+
