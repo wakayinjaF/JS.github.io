68a8b64b76452e718c9fe86f5f61cb5a86c66372
wakayinja frank <142891493+wakayinjaF@users.noreply.github.com>
2023-09-19 15:06:35 +0300

    Update New.js

--- New.js
+++ New.js
@@ -31,3 +31,51 @@ const numbers = [2, 4, 5, 6];
 console.log(numbers.length);
 
 
+// array objects
+// An array object is a collection of properties, where each property has a key (name) and value.
+let person = new Object();
+person.name = 'Daniel';
+person.eyeColor= 'Blue';
+person.age= 27;
+
+console.log(person);
+
+
+//object with function(method)
+let person = new Object();
+person.name = 'Daniel';
+person.eyeColor = 'Blue';
+person.age = 27;
+person.updateAge = function () {
+  return ++person.age;
+}
+
+console.log(person.updateAge());
+
+
+
+
+//calculating the netpay 
+let Bob = new Object();
+Bob.payRate = '15%';
+Bob.hours = 3500;
+Bob.paySalary = 800000;
+Bob.netSalary = function () {
+  return Bob.paySalary * (1 - parseFloat(Bob.payRate) / 100);
+}
+
+console.log(Bob.netSalary());
+
+
+//writting objects in a simple way 
+let person = {
+  name: 'Daniel',
+  eyeColor: 'Blue',
+  age: 27,
+  updateAge: function () {
+    return ++person.age;
+  }
+}
+
+console.log(person.age);
+
